ce6775ea2f5b14c6bafe23be1a8fb8b80b7edafe
matthewguillory65 <matthew.guillor@ad.aie.edu>
2017-02-01 17:42:29 -0600

    Tutorial on interfaces for Unity

---
+++ Assets/Scripts/StateMachine/ChaseState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : IEnemyState
+{
+    public void UpdateState()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    public void ToPatrolState()
+    {
+
+    }
+
+    public void ToAlertState()
+    {
+
+    }
+
+    public void ToChaseState()
+    {
+
+    }
+}
